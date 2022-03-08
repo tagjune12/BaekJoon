6d9f8efa295dcba54cd9670104e4fc77dce1ccb2
tagjune12 <tagjune12@gmail.com>
2022-03-08 22:25:09 +0900

    1325번 실패

---
+++ src/1325.js
@@ -0,0 +1,55 @@
+// 효율적인 해킹
+// 시간 초과
+let fs = require('fs');
+let input = fs.readFileSync('./example.txt').toString().split('\n')
+const [n, m] = input.shift().split(" ").map(v => +v);
+
+const graph = {};
+// 그래프 만들기
+for (let i = 0; i < m; i++) {
+    const [to, from] = input.shift().split(" ").map(v => +v);
+    if (graph[from] === undefined) {
+        graph[from] = [];
+    }
+    graph[from].push(to);
+}
+
+const dfs = (start) => {
+    const stack = [];
+    const visited = [];
+
+    // visited.push(start);
+    stack.push(start);
+
+    while (stack.length > 0) {
+        const curNode = stack.pop();
+        const children = graph[curNode];
+
+        if (children === undefined) continue;
+        else {
+            if (!visited.includes(curNode)) {
+                visited.push(curNode);
+                stack.push(...children);
+                // console.log(stack);
+            }
+        }
+    }
+    // console.log(visited);
+
+    return visited.length;
+}
+
+const numOfHacked = Array(n).fill(0);
+// console.log(graph);
+for (const key in graph) {
+    // console.log(key);
+    const temp = dfs(parseInt(key));
+    numOfHacked[parseInt(key)] = temp;
+}
+const max = Math.max(...numOfHacked);
+const answer = [];
+numOfHacked.forEach((value, index) => {
+    if (value === max) answer.push(index);
+})
+
+console.log(...answer);
---
+++ src/2636.js
@@ -0,0 +1,76 @@
+// 치즈
+const { count } = require('console');
+let fs = require('fs');
+let input = fs.readFileSync('./example.txt').toString().split('\n');
+const [row, col] = input.shift().split(" ").map(v => +v);
+// console.log(input);
+let matrix = input.map(arr => arr.split(" ").map(x => +x));
+
+
+// 북 동 남 서
+const dx = [0, 1, 0, -1];
+const dy = [-1, 0, 1, 0];
+
+const checkOutOfMatrix = (curRow, curCol) => {
+    // if (curRow < 0 || curCol < 0 || curRow >= row || curCol >= col) return false;
+    // else return true;
+
+    if (curRow >= 0 && curCol >= 0 && curRow < row && curCol < col) return true;
+    else return false;
+}
+const countLeftCheese = () => {
+    let count = 0;
+
+    for (let i = 0; i < row; i++) {
+        count += matrix[i].filter(value => value === 1).length;
+    }
+
+
+    return count;
+}
+
+const bfs = () => {
+    const queue = [[0, 0]];
+    const visited = Array.from(Array(row), () => Array(col).fill(false));
+    let check = false;
+    visited[0][0] = true;
+
+    while (queue.length) {
+        const [curRow, curCol] = queue.shift();
+
+        for (let i = 0; i < 4; i++) {
+            const [nextRow, nextCol] = [curRow + dx[i], curCol + dy[i]];
+
+            if (checkOutOfMatrix(nextRow, nextCol) && visited[nextRow][nextCol] === false) {
+                if (matrix[nextRow][nextCol] === 0) {
+                    visited[nextRow][nextCol] = true;
+                    queue.push([nextRow, nextCol]);
+                }
+                if (matrix[nextRow][nextCol] === 1) {
+                    check = true;
+                    visited[nextRow][nextCol] = true;
+                    matrix[nextRow][nextCol] = 0;
+                }
+            }
+        }
+    }
+
+    return check;
+}
+
+let hours = 0;
+let lastCheese = 0;
+
+while (true) {
+    const temp = countLeftCheese();
+    if (temp !== 0) {
+        lastCheese = temp;
+    }
+    if (bfs()) {
+        hours++;
+    }
+    else break;
+}
+
+console.log(hours);
+console.log(lastCheese);
