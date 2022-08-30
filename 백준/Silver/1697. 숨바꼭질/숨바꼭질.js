15aa3e8f9ab7b930da936567f4a5ff731158258b
tagjune12 <54172578+tagjune12@users.noreply.github.com>
2022-08-31 00:14:20 +0900

    [Silver I] Title: 숨바꼭질, Time: 252 ms, Memory: 26864 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,32 +1,36 @@
-// 숨바꼭질
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const [n, k] = input.shift().split(" ").map(v => +v);
+const MIN = 0;
+const MAX = 100000;
+const [me, bro] = require('fs').readFileSync('dev/stdin').toString().trim().split(' ').map(v => +v);
 
-const solution = (n, k) => {
+function solution() {
 
-    const dx = [-1, 1, 2];
+    const bfs = (start, end) => {
+        if (end < start) {
+            return start - end;
+        }
 
-    const bfs = (n, k) => {
-        const queue = [[n, 0]];
-        const visited = Array(100001).fill(false);
+        const queue = [[start, 0]];
+        const visited = Array(MAX + 1).fill(false);
 
-        for (let i = 0; i < queue.length; i++) {
-            const [curPos, count] = queue[i];
-            if (curPos === k) return count;
+        for (let j = 0; j < queue.length; j++) {
+            const [cur, count] = queue[j];
+            if (cur === end) return count;
+            const move = [cur - 1, cur + 1, cur * 2];
 
-            for (let j = 0; j < dx.length; j++) {
-                let nextPos = 0;
-                if (j === 2) nextPos = 2 * curPos;
-                else nextPos = curPos + dx[j];
-                if (nextPos >= 0 && nextPos <= 100000 && (visited[nextPos] === false)) {
-                    visited[nextPos] = true;
-                    queue[queue.length] = [nextPos, count + 1];
+            for (let i = 0; i < 3; i++) {
+                const pos = move[i];
+                // end보다 뒤에 있는경우 -1만 하도록
+                if (end < cur && i !== 0) continue;
+
+                // 좌표*2위치와 목적지사이의 거리랑 현재 위치랑 목적지 거리보다 멀경우 스킵
+                if (i === 2 && Math.abs(end - cur) < Math.abs(end - cur * 2)) continue;
+                if (pos >= MIN && pos <= MAX && !visited[pos]) {
+                    queue.push([pos, count + 1]);
+                    visited[pos] = true;
                 }
             }
         }
     }
-
-    console.log(bfs(n, k));
+    console.log(bfs(me, bro));
 }
-
-solution(n, k);
+solution();
