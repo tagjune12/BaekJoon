82bb721ef0398ec73bd97cf00512e06fd6fcfdfc
tagjune12 <54172578+tagjune12@users.noreply.github.com>
2022-09-01 18:19:00 +0900

    [Silver II] Title: 색종이 만들기, Time: 184 ms, Memory: 12160 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.js
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.js
@@ -3,57 +3,36 @@ let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n'
 function solution() {
   const MAX_SIZE = +input.shift();
   const answer = [0, 0];
-  let paper = [];
+  const paper = [];
   input.forEach(row => {
     paper.push(row.split(' ').map(v => +v));
   });
 
-  const isValid = (start, row, col, size) => {
-    return (row >= start[0] && col >= start[1] && row < (start[0] + size) && col < (start[1] + size))
-  }
+  const recursive = (start, size) => {
+    const color = paper[start[0]][start[1]];
+    if (size === 1) {
+      answer[color]++;
+      return;
+    }
 
-  const bfs = (start, color, size) => {
-    const queue = [start];
-    const visited = Array.from(Array(MAX_SIZE), () => Array(MAX_SIZE).fill(false));
-    visited[start[0]][start[1]] = true;
-    const dx = [0, 0, 1, -1];
-    const dy = [1, -1, 0, 0];
-    while (queue.length > 0) {
-      const [curRow, curCol] = queue.shift();
-      if (paper[curRow][curCol] !== color) {
-        return false
-      };
+    for (let i = start[0]; i < start[0] + size; i++) {
+      for (let j = start[1]; j < start[1] + size; j++) {
+        if (color !== paper[i][j]) {
+          const halfSize = Math.floor(size / 2);
+          recursive(start, halfSize);
+          recursive([start[0], start[1] + halfSize], halfSize);
+          recursive([start[0] + halfSize, start[1]], halfSize);
+          recursive([start[0] + halfSize, start[1] + halfSize], halfSize);
 
-      for (let i = 0; i < 4; i++) {
-        const [nextRow, nextCol] = [curRow + dy[i], curCol + dx[i]];
-        if (isValid(start, nextRow, nextCol, size) && !visited[nextRow][nextCol]) {
-          queue.push([nextRow, nextCol]);
-          visited[nextRow][nextCol] = true;
+          return;
         }
       }
     }
-    return true;
+
+    answer[color]++;
   }
 
-  let curSize = MAX_SIZE;
-  while (curSize >= 1) {
-    for (let i = 0; i < MAX_SIZE; i += curSize) {
-      for (let j = 0; j < MAX_SIZE; j += curSize) {
-        if (paper[i][j] === 3) continue;
-
-        if (bfs([i, j], paper[i][j], curSize)) {
-          answer[paper[i][j]]++;
-          for (let row = i; row < (i + curSize); row++) {
-            for (let col = j; col < (j + curSize); col++) {
-              paper[row][col] = 3;
-            }
-          }
-        }
-      }
-    }
-    curSize = Math.floor(curSize / 2);
-  }
-
+  recursive([0, 0], MAX_SIZE);
   console.log(`${answer[0]}\n${answer[1]}`);
 }
 
