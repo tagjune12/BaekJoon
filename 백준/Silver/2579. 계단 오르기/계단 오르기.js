28ddb75520465dd5fde1eb1e54bfc855782361e8
tagjune12 <54172578+tagjune12@users.noreply.github.com>
2022-10-12 18:59:29 +0900

    [Silver III] Title: 계단 오르기, Time: 120 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.js
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -5,10 +5,7 @@ function solution() {
   // 계단오르기
   const n = +input[0];
   const stairs = input.map(v => +v);
-  const dp = Array.from(n+1, ()=>0);
-  dp[1] = stairs[1];
-  dp[2] = dp[1] + stairs[2];
-  dp[3] = Math.max(stairs[1], stairs[2]) + stairs[3];
+  const dp = [0, stairs[1], stairs[1] + stairs[2], Math.max(stairs[1], stairs[2]) + stairs[3]];
 
   for (let i = 4; i <= n; i++) {
     dp[i] = Math.max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i]);
