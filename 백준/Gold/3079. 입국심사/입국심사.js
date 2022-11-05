0a38c1ae4ce3a72abc5ab220143034e2d1237b88
tagjune12 <54172578+tagjune12@users.noreply.github.com>
2022-11-05 19:50:11 +0900

    [Gold V] Title: 입국심사, Time: 348 ms, Memory: 32648 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.js
+++ 백준/Gold/3079. 입국심사/입국심사.js
@@ -1,27 +1,32 @@
-function solution(m, arr) {
-  if (arr.length === 1) {
-    console.log(arr[0] * m);
+const path = process.platform === 'linux' ? 'dev/stdin' : './src/example.txt'
+let input = require('fs').readFileSync(path).toString().trim().split('\n');
+
+function solution() {
+  const [N, M] = input[0].split(' ').map(v => +v);
+  const desk = input.slice(1).map(v => +v);
+
+  if (desk.length === 1) {
+    console.log(desk[0] * M);
     return;
   }
-  let start = 0;
-  let end = m * Math.max(...arr)
-  while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
-    let total = arr.reduce((acc, cur) => acc + Math.floor(mid / cur), 0);
-    if (total >= m) end = mid - 1;
-    else start = mid + 1;
+
+  let left = 1;
+  let right = Math.max(...desk) * M;
+  let answer = Number.MAX_SAFE_INTEGER;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const temp = desk.reduce((acc, cost) => acc + Math.floor(mid / cost), 0);
+
+    if (temp >= M) {
+      right = mid - 1;
+      answer = Math.min(answer, mid);
+    } else {
+      left = mid + 1;
+    }
   }
-  console.log(end + 1);
+
+  console.log(answer);
 }
 
-
-const readline = require("readline");
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-let input = [];
-rl.on("line", function (line) {
-  input.push(line);
-}).on("close", function () {
-  const [N, M] = input[0].split(" ").map(Number);
-  const arr = input.slice(1).map(Number);
-  solution(M, arr);
-});
+solution();
