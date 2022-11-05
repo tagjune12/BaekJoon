0153a25f9b2568e52b389b372e9649e70dddbd43
tagjune12 <54172578+tagjune12@users.noreply.github.com>
2022-11-05 20:16:44 +0900

    [Gold V] Title: 한글 LCS, Time: 240 ms, Memory: 24688 KB -BaekjoonHub

---
+++ 백준/Gold/15482. 한글 LCS/한글 LCS.js
@@ -0,0 +1,22 @@
+const path = process.platform === 'linux' ? 'dev/stdin' : './src/example.txt'
+let input = require('fs').readFileSync(path).toString().trim().split('\n');
+
+function solution() {
+  const str1 = input.shift().trim();
+  const str2 = input.shift().trim();
+
+  const arr = Array.from(Array(str1.length + 1), () => Array(str2.length + 1).fill(0));
+  for (let i = 1; i <= str1.length; i++) {
+    for (let j = 1; j <= str2.length; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        arr[i][j] = arr[i - 1][j - 1] + 1;
+      } else {
+        arr[i][j] = Math.max(arr[i - 1][j], arr[i][j - 1]);
+      }
+    }
+  }
+
+  console.log(arr[str1.length][str2.length]);
+}
+
+solution();
